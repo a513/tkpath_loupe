d94eebc0e299884f48a34b3d36b31e4dc9989d19
hvxl <hvxl@tclcode.com>
2026-04-04 17:20:23 +0200

    Update the build environment.

--- generic/tkIntPath.h
+++ generic/tkIntPath.h
@@ -21,21 +21,6 @@
 extern "C" {
 #endif
 
-/* Check, if Tcl version supports Tcl_Size,
-   which was introduced in Tcl 8.7 and 9.
-*/
-#ifndef TCL_SIZE_MAX
-    #include <limits.h>
-    #define TCL_SIZE_MAX INT_MAX
-
-    #ifndef Tcl_Size
-        typedef int Tcl_Size;
-    #endif
-
-    #define TCL_SIZE_MODIFIER ""
-    #define Tcl_GetSizeIntFromObj Tcl_GetIntFromObj
-#endif
-
 /*
  * From tclInt.h version 1.118.2.11
  * Ensure WORDS_BIGENDIAN is defined correcly:
